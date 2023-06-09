7dc14d745c3e32780dcbaf57f79de7fc35c0e40b
MeshachArinze <meshacharinze@gmail.com>
2023-06-09 21:26:49 +0100

    commit to repo

---
+++ todoapp/pages/index.js
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import App from '../components/App';
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>todo App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <App />
+      </main>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
