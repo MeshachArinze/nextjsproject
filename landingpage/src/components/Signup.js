a70bd1a85df886d2370497b8be33e021ed5f18ca
MeshachArinze <meshacharinze@gmail.com>
2023-06-10 01:13:46 +0100

    commit to repo

--- landingpage/src/components/Signup.js
+++ landingpage/src/components/Signup.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import Link from "next/link";
 import Connect from "./Connect";
 import Footer from "./Footer";
 import Form from "./Form";
