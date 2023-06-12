f070a5dc8ad7e401014c73ed08ade3c9b3a1fd5e
MeshachArinze <meshacharinze@gmail.com>
2023-06-12 14:13:23 +0100

    commit to repo

--- leetcode/src/components/Workspace/Workspace.tsx
+++ leetcode/src/components/Workspace/Workspace.tsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState} from 'react'
 import Split from "react-split";
 import Confetti from "react-confetti"
 import ProblemDescription from "./ProblemDescription/ProblemDescription";
 import Playground from "./Playground/Playground";
 import { Problem } from "@/utils/types/problem";
-import useWindowSize from '@/hooks/useWindowSize'
+import useWindowSize from '@/hooks/useWindowSize';
 
-;
-const Workspace = () => {
+type WorkspaceProps = {
+  problem: Problem;
+}
+
+const Workspace: React.FC<WorkspaceProps> = ({problem}) => {
+  const { width, height} = useWindowSize();
+  const [success, setSuccess] = useState(false);
+  const [solved, setSolved] = useState(false);
   return (
-    <div>Workspace</div>
-  )
+    <Split>
+      <ProblemDescription problem={problem} _solved={solved} />
+      <div className="bg-dark-fill-2">
+        <Playground
+          problem={problem}
+          setSuccess={setSuccess}
+          setSolved={setSolved}
+        />
+        {success && (
+          <Confetti
+            gravity={0.3}
+            tweenDuration={4000}
+            width={width - 1}
+            height={height - 1}
+          />
+        )}
+      </div>
+    </Split>
+  );
 }
 
 export default Workspace
